68ed0b51a2e75b5b22dc3f4eec3faad82743ef40
RubinhoCR7 <99280508+RubinhoCR7@users.noreply.github.com>
2022-11-13 21:49:37 +0100

    exo 5 finito pipo doublé d'olivier Giroud

--- exercice5.js
+++ exercice5.js
@@ -6,21 +6,21 @@ import java.util.Arrays;
 
  		public static void main(String[] args) {
 
- 	//init tableau
+ 			//init tableau
  			int array[] = {6, 2, 76, 12, 7, 100};
 
- 	//afficher tous les nombres
- 		for (int entier : array) {
- 			System.out.println("nombre: " + entier);
- 	}
-
- 	//trier dans le tableau
- 			Arrays.sort(array);
-
- 	//afficher les nombres dans l'odre après le tri
- 			System.out.println("Tableau trié\n");
- 		for (int entier : array) {
+ 			//afficher tous les nombres
+ 			for (int entier : array) {
  			System.out.println("nombre: " + entier);
  			}
+
+ 			//trier dans le tableau
+ 			Arrays.sort(array);
+
+ 			//afficher les nombres dans l'odre après le tri
+ 			System.out.println("Tableau trié\n");
+ 			for (int entier : array) {
+ 			System.out.println("nombre: " + entier);
  				}
-	}
+ 							}
+		}
