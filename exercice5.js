847ebee4d9e2773cf1f1deaac97f494e693c9cf8
RubinhoCR7 <99280508+RubinhoCR7@users.noreply.github.com>
2022-11-13 21:45:16 +0100

    Update exercice5.js

--- exercice5.js
+++ exercice5.js
@@ -7,20 +7,20 @@ import java.util.Arrays;
  		public static void main(String[] args) {
 
  	//init tableau
- 	int array[] = {6, 2, 76, 12, 7, 100};
+ 			int array[] = {6, 2, 76, 12, 7, 100};
 
  	//afficher tous les nombres
- 	for (int entier : array) {
- 	System.out.println("nombre: " + entier);
+ 		for (int entier : array) {
+ 			System.out.println("nombre: " + entier);
  	}
 
  	//trier dans le tableau
- 	Arrays.sort(array);
+ 			Arrays.sort(array);
 
  	//afficher les nombres dans l'odre après le tri
- 	System.out.println("Tableau trié\n");
- 	for (int entier : array) {
- 	System.out.println("nombre: " + entier);
- 	}
- 		}
+ 			System.out.println("Tableau trié\n");
+ 		for (int entier : array) {
+ 			System.out.println("nombre: " + entier);
+ 			}
+ 				}
 }
