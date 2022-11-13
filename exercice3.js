1d117996e0d4a1711980bee5896fb393f6f1d23a
Ruben Gaches <ruben@MacBook-Pro-de-Ruben.local>
2022-11-13 14:49:43 +0100

    exercice3

---
+++ exercice3.js
@@ -0,0 +1,29 @@
+//interagir avec le packageeeeee
+
+const prompt = require("prompt-sync")();
+
+
+function ajouter10() {
+
+	//choissir nombreeeeee
+
+    let inputNumber = prompt("Choisir votre nombre : ");
+
+    //transformer la chaine de caractères en nombreeeeee
+
+    let inputToInt = parseInt(inputNumber)
+
+    if(isNaN(inputToInt)){
+        console.log("It's not a figure")
+        ajouter10()
+    }else{
+        let maxNumber = inputToInt + 10
+        for(let i = 0; inputToInt != maxNumber; i++){
+            inputToInt++;
+            console.log(inputToInt);
+        }
+    }
+}
+ 
+ajouter10();
+
