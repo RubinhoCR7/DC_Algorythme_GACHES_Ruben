f80b1a65871f497dd456d706d19157ead9f54333
RubinhoCR7 <99280508+RubinhoCR7@users.noreply.github.com>
2022-11-13 22:02:29 +0100

    exo 4 finito leonardo bonucci

---
+++ exercice4.js
@@ -0,0 +1,12 @@
+function start(x){
+
+    var calculNouvelleValeure = 0
+
+    for(var i = 1; i <= x ; i++){
+        calculNouvelleValeur= calculNouvelleValeur + i;
+    }
+
+    calculNouvelleValeur;
+}
+
+console.log(start(5));
