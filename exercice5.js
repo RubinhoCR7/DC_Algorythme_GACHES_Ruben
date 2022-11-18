3bfc42bf24a49da5944aa409db16159c4eb13102
RubinhoCR7 <99280508+RubinhoCR7@users.noreply.github.com>
2022-11-18 10:35:58 +0100

    exo5.js refait hat trick d'Olivier Giroud

--- exercice5.js
+++ exercice5.js
@@ -1,26 +1,15 @@
-exercice5.js
-
-import java.util.Arrays;
-
-	public class ArraysTest {
-
- 		public static void main(String[] args) {
-
- 			//init tableau
- 			int array[] = {6, 2, 76, 12, 7, 100};
-
- 			//afficher tous les nombres
- 			for (int entier : array) {
- 			System.out.println("nombre: " + entier);
- 			}
-
- 			//trier dans le tableau
- 			Arrays.sort(array);
-
- 			//afficher les nombres dans l'odre après le tri
- 			System.out.println("Tableau trié\n");
- 			for (int entier : array) {
- 			System.out.println("nombre: " + entier);
- 				}
- 							}
-		}
+function sortIncreaseArray(data) {
+    if(data != "" || data != null || data != []) {
+        for (let i = 0; i < data.length; i++) {
+            for (let j = i + 1; j < data.length; j++) {
+                if (data[i] > data[j]) {
+                  temp = data[i];
+                  data[i] = data[j];
+                  data[j] = temp;
+                }
+            }
+        }
+        return data;
+    }
+    return null;
+}
